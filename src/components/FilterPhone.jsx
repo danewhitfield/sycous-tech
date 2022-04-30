3ed6e38c11e831ecb5223c744ae3a026d09c996d
Dane <danejwhitfield@gmail.com>
2022-04-30 19:39:52 +0100

    updated proxy url

--- src/components/FilterPhone.jsx
+++ src/components/FilterPhone.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
 import { StyledLink } from '../styles/Link.style'
 import { StyledList } from '../styles/List.style'
 import DropDown from './DropDown.jsx'
